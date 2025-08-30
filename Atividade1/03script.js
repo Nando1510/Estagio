95ef417356a8cade875b0879a4e4436a78e83961
Nando1510 <117784292+Nando1510@users.noreply.github.com>
2025-08-30 17:04:26 -0300

    Update 03script.js

--- Atividade1/03script.js
+++ Atividade1/03script.js
@@ -4,58 +4,52 @@ function limpar_Inputs(){
 
 }
 
-//NÃO É NECESSÁRIO
-//const successBox = document.getElementById('success-box'); // CORREÇÃO: referência à caixinha
-
 const botao_Enviar = document.getElementById('enviar');
 botao_Enviar.addEventListener('click', envio);
 
-function envio(event) {
+async function envio(event){
     event.preventDefault();
-
-    document.getElementById('error-nome').textContent = '';
-    document.getElementById('error-email').textContent = '';
-    document.getElementById('error-senha').textContent = '';
-    document.getElementById('success-box').style.display = 'none';
-   
 
     const nome = document.getElementById('nome').value.trim();
     const email = document.getElementById('email').value.trim();
     const senha = document.getElementById('senha').value.trim();
 
-    let valido = true;
+    document.getElementById('error-nome').textContent = '';
+    document.getElementById('error-email').textContent = '';
+    document.getElementById('error-senha').textContent = '';
 
-    if (nome === "") {
-        document.getElementById('error-nome').textContent = "O campo Nome é obrigatório.";
+    document.getElementById('sucesso').style.display = 'none';
 
-        valido = false;
+    let validar = true;
+
+    if (!nome){
+        document.getElementById('error-nome').textContent = 'Preenchimento obrigatorio do campo nome!!!';
+        validar = false;
     }
 
-    if (email === "") {
-    document.getElementById('error-email').textContent = "O campo email é obrigatório.";
-    valido = false;
-    } else if (!email.includes("@")) {
-            document.getElementById('error-email').textContent = "Digite um e-mail válido.";
-            valido = false;
+    if (!email){
+        document.getElementById('error-email').textContent = 'Preenchimento obrigatório do  campo e-mail!!!';
+        validar = false;
+    }
+    else if (!email.includes('@')){
+        document.getElementById('error-email').textContent = 'E-mail inválido!!!';
+        validar = false;
     }
 
-
-    if (senha === "" ){
-        document.getElementById('error-senha').textContent = "O campo senha é obrigatório."
-        valido = false
+    if (!senha){
+        document.getElementById('error-senha').textContent = 'Preenchimento obrigatório do campo senha!!!';
+        validar = false;
     }
 
-    if (senha.length < 6){
-        document.getElementById('error-senha').textContent = "O campo senha deve conter pelo menos  6 caracteres."
-        valido = false;
+    else if (senha.length < 6){
+        document.getElementById('error-senha').textContent = 'Sua senha deve de conter no mínimo 6 caracteres!!!';
+        validar = false;
     }
 
-    if (valido) {
-        document.getElementById("success-box").style.display = "block";
-        successBox.textContent = "Cadastro realizado com sucesso!";
-        
+    if (validar){
+        document.getElementById('sucesso').style.display = 'block';
+
         limpar_Inputs();
+
     }
- 
-    
 }
